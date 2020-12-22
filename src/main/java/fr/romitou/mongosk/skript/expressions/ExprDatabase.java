d37119a644be16352e92467a2f04e269555c149f
DeltaRays <42403698+DeltaRays@users.noreply.github.com>
2020-12-22 06:57:31 +0100

    Update src/main/java/fr/romitou/mongosk/skript/expressions/ExprDatabase.java
    Co-authored-by: Olyno <olyno.dev@gmail.com>

--- src/main/java/fr/romitou/mongosk/skript/expressions/ExprDatabase.java
+++ src/main/java/fr/romitou/mongosk/skript/expressions/ExprDatabase.java
@@ -46,7 +46,7 @@ public class ExprDatabase extends SimpleExpression<MongoDatabase> {
                     ? new MongoDatabase[0]
                     : new MongoDatabase[]{client.getDatabase(name)};
         } catch (IllegalArgumentException ex) {
-            return null; // A MongoDB database with that name doesn't exist!
+            return new MongoDatabase[0]; // A MongoDB database with that name doesn't exist!
         }
     }
 
