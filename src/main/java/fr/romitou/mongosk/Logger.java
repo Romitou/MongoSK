4f39a063fb5b061d6f1e8d2200fffdb4dd271766
Romitou <romitou@protonmail.com>
2021-02-09 21:08:39 +0100

    :loud_sound: Improve Logger

--- src/main/java/fr/romitou/mongosk/Logger.java
+++ src/main/java/fr/romitou/mongosk/Logger.java
@@ -7,37 +7,48 @@ import java.util.Arrays;
 public class Logger {
 
     private final static String PREFIX = "&7[&2Mongo&aSK&7] ";
+    private final static Boolean DEBUG = MongoSK.getConfiguration().getBoolean("debug", false);
 
     public static String getFormattedString(String message) {
         return ChatColor.translateAlternateColorCodes('&', PREFIX + message);
     }
 
-    public static void info(String... message) {
-        System.out.println(getFormattedString(ChatColor.WHITE + message[0]));
-        if (message.length > 1)
-            Arrays.stream(message).forEachOrdered(Logger::addDebugInfo);
+    public static void info(String... messages) {
+        System.out.println(getFormattedString(ChatColor.WHITE + messages[0]));
+        if (messages.length > 1)
+            Arrays.stream(messages)
+                .skip(1)
+                .forEachOrdered(Logger::addDebugInfo);
     }
 
-    public static void debug(String... message) {
-        System.out.println(getFormattedString(ChatColor.GRAY + message[0]));
-        if (message.length > 1)
-            Arrays.stream(message).forEachOrdered(msg -> System.out.println(getFormattedString(ChatColor.GOLD + "→ " + msg)));
+    public static void debug(String... messages) {
+        if (!DEBUG) return;
+        System.out.println(getFormattedString(ChatColor.DARK_GRAY + messages[0]));
+        if (messages.length > 1)
+            Arrays.stream(messages)
+                .skip(1)
+                .forEachOrdered(msg -> System.out.println(getFormattedString(ChatColor.DARK_GRAY + "-> " + msg)));
     }
 
     private static void addDebugInfo(String message) {
-        System.out.println(getFormattedString(ChatColor.GOLD + "→ " + message));
+        if (!DEBUG) return;
+        System.out.println(getFormattedString(ChatColor.DARK_GRAY + "-> " + message));
     }
 
-    public static void warn(String... message) {
-        System.out.println(getFormattedString(ChatColor.GOLD + message[0]));
-        if (message.length > 1)
-            Arrays.stream(message).forEachOrdered(Logger::addDebugInfo);
+    public static void warn(String... messages) {
+        System.out.println(getFormattedString(ChatColor.GOLD + messages[0]));
+        if (messages.length > 1)
+            Arrays.stream(messages)
+                .skip(1)
+                .forEachOrdered(Logger::addDebugInfo);
     }
 
-    public static void severe(String... message) {
-        System.out.println(getFormattedString(ChatColor.RED + message[0]));
-        if (message.length > 1)
-            Arrays.stream(message).forEachOrdered(Logger::addDebugInfo);
+    public static void severe(String... messages) {
+        System.out.println(getFormattedString(ChatColor.RED + messages[0]));
+        if (messages.length > 1)
+            Arrays.stream(messages)
+                .skip(1)
+                .forEachOrdered(Logger::addDebugInfo);
     }
 
 }
