9380307544936e6897ca6544be87805fe85a067c
Romitou <romitou@protonmail.com>
2021-02-10 15:00:25 +0100

    :sparkles: Add Skript types

--- src/main/java/fr/romitou/mongosk/skript/SkriptTypes.java
+++ src/main/java/fr/romitou/mongosk/skript/SkriptTypes.java
@@ -4,7 +4,7 @@ import ch.njol.skript.classes.ClassInfo;
 import ch.njol.skript.classes.Parser;
 import ch.njol.skript.lang.ParseContext;
 import ch.njol.skript.registrations.Classes;
-import fr.romitou.mongosk.elements.MongoSKServer;
+import fr.romitou.mongosk.elements.*;
 
 import javax.annotation.Nonnull;
 
@@ -40,5 +40,130 @@ public class SkriptTypes {
                 }
             })
         );
+
+        Classes.registerClass(new ClassInfo<>(MongoSKDatabase.class, "mongoskdatabase")
+            .user("mongo(db|sk)?( |-)?databases?")
+            .name("MongoSK Database")
+            .description("Represents a Mongo database, which contains collections. If you want to create this type, look at the Mongo Database expression.")
+            .since("2.0.0")
+            .parser(new Parser<MongoSKDatabase>() {
+                @Override
+                public boolean canParse(@Nonnull ParseContext context) {
+                    return false;
+                }
+
+                @Override
+                @Nonnull
+                public String toString(@Nonnull MongoSKDatabase database, int flags) {
+                    return database.getMongoDatabase().getName() + " database";
+                }
+
+                @Override
+                @Nonnull
+                public String toVariableNameString(@Nonnull MongoSKDatabase database) {
+                    return "mongoskdatabase:" + database.getMongoDatabase().getName();
+                }
+
+                @Override
+                @Nonnull
+                public String getVariableNamePattern() {
+                    return "mongoskdatabase:.+";
+                }
+            })
+        );
+
+        Classes.registerClass(new ClassInfo<>(MongoSKCollection.class, "mongoskcollection")
+            .user("mongo(db|sk)?( |-)?collections?")
+            .name("MongoSK Collection")
+            .description("Represents a Mongo collection, which contains documents. Use the Mongo Collection expression to create this type.")
+            .since("2.0.0")
+            .parser(new Parser<MongoSKCollection>() {
+                @Override
+                public boolean canParse(@Nonnull ParseContext context) {
+                    return false;
+                }
+
+                @Override
+                @Nonnull
+                public String toString(@Nonnull MongoSKCollection collection, int flags) {
+                    return collection.getMongoCollection().getNamespace().getCollectionName() + " collection";
+                }
+
+                @Override
+                @Nonnull
+                public String toVariableNameString(@Nonnull MongoSKCollection collection) {
+                    return "mongoskcollection:" + collection.getMongoCollection().getNamespace().getCollectionName();
+                }
+
+                @Override
+                @Nonnull
+                public String getVariableNamePattern() {
+                    return "mongoskcollection:.+";
+                }
+            })
+        );
+
+        Classes.registerClass(new ClassInfo<>(MongoSKDocument.class, "mongoskdocument")
+            .user("mongo(db|sk)?( |-)?documents?")
+            .name("MongoSK Document")
+            .description("Represents a Mongo (BSON) document, which contains some fields and values. If you want to read fields values, use the Mongo Value expression. Otherwise, you can use the Mongo Query expression to query your Mongo collection for documents.")
+            .since("2.0.0")
+            .parser(new Parser<MongoSKDocument>() {
+                @Override
+                public boolean canParse(@Nonnull ParseContext context) {
+                    return false;
+                }
+
+                @Override
+                @Nonnull
+                public String toString(@Nonnull MongoSKDocument document, int flags) {
+                    return document.getBsonDocument().toJson() + " document";
+                }
+
+                @Override
+                @Nonnull
+                public String toVariableNameString(@Nonnull MongoSKDocument document) {
+                    return "mongoskdocument:" + document.getBsonDocument().toJson();
+                }
+
+                @Override
+                @Nonnull
+                public String getVariableNamePattern() {
+                    return "mongoskdocument:.+";
+                }
+            })
+        );
+
+        Classes.registerClass(new ClassInfo<>(MongoSKFilter.class, "mongoskfilter")
+            .user("mongo(db|sk)?( |-)?filters?")
+            .name("MongoSK Filter")
+            .description("Represents a filter, which can be used in your queries to target a certain type of data. You can create a filter using the Mongo Filter expression. ")
+            .since("2.0.0")
+            .parser(new Parser<MongoSKFilter>() {
+                @Override
+                public boolean canParse(@Nonnull ParseContext context) {
+                    return false;
+                }
+
+                @Override
+                @Nonnull
+                public String toString(@Nonnull MongoSKFilter filter, int flags) {
+                    return filter.toString();
+                }
+
+                @Override
+                @Nonnull
+                public String toVariableNameString(@Nonnull MongoSKFilter filter) {
+                    return "mongoskfilter:" + filter.getFilter().toBsonDocument().toJson();
+                }
+
+                @Override
+                @Nonnull
+                public String getVariableNamePattern() {
+                    return "mongoskfilter:.+";
+                }
+            })
+        );
+
     }
 }
