e30ff1ec341a9bbf3aa9a5db208aad146c5508da
Romitou <romitou@protonmail.com>
2021-02-19 15:24:46 +0100

    :building_construction: Saving ItemStack and ItemType in binary data

--- src/main/java/fr/romitou/mongosk/adapters/codecs/ItemStackCodec.java
+++ src/main/java/fr/romitou/mongosk/adapters/codecs/ItemStackCodec.java
@@ -1,16 +1,16 @@
 package fr.romitou.mongosk.adapters.codecs;
 
 import ch.njol.skript.Skript;
+import ch.njol.skript.classes.ClassInfo;
+import ch.njol.skript.registrations.Classes;
+import ch.njol.skript.variables.SerializedVariable;
 import fr.romitou.mongosk.adapters.MongoSKCodec;
 import org.bson.Document;
-import org.bukkit.Material;
+import org.bson.types.Binary;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.Damageable;
 
 import javax.annotation.Nonnull;
 import java.io.StreamCorruptedException;
-import java.util.Arrays;
-import java.util.List;
 
 public class ItemStackCodec implements MongoSKCodec<ItemStack> {
 
@@ -19,49 +19,24 @@ public class ItemStackCodec implements MongoSKCodec<ItemStack> {
     @Nonnull
     @Override
     public ItemStack deserialize(Document document) throws StreamCorruptedException {
-        Integer amount = document.getInteger("amount"),
-            customModelData = document.getInteger("customModelData"),
-            damage = document.getInteger("damage");
-        String displayName = document.getString("displayName"),
-            materialName = document.getString("material");
-        List<String> lore = document.getList("lore", String.class);
-        if (materialName == null)
-            throw new StreamCorruptedException("Cannot deserialize material from document!");
-        System.out.println(Arrays.toString(Material.values()));
-        Material material;
-        if (IS_LEGACY) {
-            material = Material.valueOf(materialName);
-        } else {
-           material = Material.getMaterial(materialName);
-           if (material == null) // Try to get the legacy material...
-               material = Material.getMaterial(materialName, true);
-        }
-        if (material == null)
-            throw new StreamCorruptedException("Given material name is invalid!");
-        ItemStack baseItemStack = new ItemStack(material, amount);
-        if (customModelData != null)
-            baseItemStack.getItemMeta().setCustomModelData(customModelData);
-        if (damage != null)
-            ((Damageable) baseItemStack.getItemMeta()).setDamage(damage);
-        if (displayName != null)
-            baseItemStack.getItemMeta().setDisplayName(displayName);
-        if (lore != null)
-            baseItemStack.getItemMeta().setLore(lore);
-        return baseItemStack;
+        Binary binary = (Binary) document.get("binary");
+        ClassInfo<?> classInfo = Classes.getExactClassInfo(ItemStack.class);
+        if (binary == null || classInfo == null)
+            throw new StreamCorruptedException("Cannot retrieve binary field or ItemStack class info!");
+        Object deserialized = Classes.deserialize(classInfo, binary.getData());
+        if (!(deserialized instanceof ItemStack))
+            throw new StreamCorruptedException("Deserialized object is not an ItemStack!");
+        return (ItemStack) deserialized;
     }
 
     @Nonnull
     @Override
     public Document serialize(ItemStack itemStack) {
         Document document = new Document();
-        document.put("material", itemStack.getType().name());
-        document.put("amount", itemStack.getAmount());
-        if (itemStack instanceof Damageable)
-            document.put("damage", ((Damageable) itemStack.getItemMeta()).getDamage());
-        if (itemStack.getItemMeta().hasCustomModelData())
-            document.put("customModelData", itemStack.getItemMeta().getCustomModelData());
-        document.put("displayName", itemStack.getItemMeta().getDisplayName());
-        document.put("lore", itemStack.getLore());
+        SerializedVariable.Value serialized = Classes.serialize(itemStack);
+        if (serialized == null)
+            return new Document();
+        document.put("binary", serialized.data);
         return document;
     }
 
--- src/main/java/fr/romitou/mongosk/adapters/codecs/ItemTypeCodec.java
+++ src/main/java/fr/romitou/mongosk/adapters/codecs/ItemTypeCodec.java
@@ -1,28 +1,43 @@
 package fr.romitou.mongosk.adapters.codecs;
 
 import ch.njol.skript.aliases.ItemType;
+import ch.njol.skript.classes.ClassInfo;
+import ch.njol.skript.registrations.Classes;
+import ch.njol.skript.variables.SerializedVariable;
 import fr.romitou.mongosk.adapters.MongoSKCodec;
 import org.bson.Document;
-import org.bukkit.inventory.ItemStack;
+import org.bson.types.Binary;
 
 import javax.annotation.Nonnull;
 import java.io.StreamCorruptedException;
 
+/**
+ * This class uses binaries fields with Skript serializers and deserializers in order to take up less database space.
+ * Serialising fields in plain text like other codecs would be useless and counterproductive.
+ */
 public class ItemTypeCodec implements MongoSKCodec<ItemType> {
     @Nonnull
     @Override
     public ItemType deserialize(Document document) throws StreamCorruptedException {
-        // Possible errors are always thrown by ItemStackCodec.
-        return new ItemType(new ItemStackCodec().deserialize(document));
+        Binary binary = (Binary) document.get("binary");
+        ClassInfo<?> classInfo = Classes.getExactClassInfo(ItemType.class);
+        if (binary == null || classInfo == null)
+            throw new StreamCorruptedException("Cannot retrieve binary field or ItemType class info!");
+        Object deserialized = Classes.deserialize(classInfo, binary.getData());
+        if (!(deserialized instanceof ItemType))
+            throw new StreamCorruptedException("Deserialized object is not an ItemType!");
+        return (ItemType) deserialized;
     }
 
     @Nonnull
     @Override
     public Document serialize(ItemType itemType) {
-        ItemStack randomItemStack = itemType.getRandom();
-        if (randomItemStack == null)
+        Document document = new Document();
+        SerializedVariable.Value serialized = Classes.serialize(itemType);
+        if (serialized == null)
             return new Document();
-        return new ItemStackCodec().serialize(randomItemStack);
+        document.put("binary", serialized.data);
+        return document;
     }
 
     @Nonnull
