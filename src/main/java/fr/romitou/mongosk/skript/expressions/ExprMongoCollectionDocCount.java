3605419a5e56156a41b8a3d5d58d65188289c54f
Romitou <romitou@protonmail.com>
2021-02-20 00:40:01 +0100

    :sparkles: Add Mongo collection document count

---
+++ src/main/java/fr/romitou/mongosk/skript/expressions/ExprMongoCollectionDocCount.java
@@ -0,0 +1,34 @@
+package fr.romitou.mongosk.skript.expressions;
+
+import ch.njol.skript.expressions.base.SimplePropertyExpression;
+import fr.romitou.mongosk.SubscriberHelpers;
+import fr.romitou.mongosk.elements.MongoSKCollection;
+
+import javax.annotation.Nonnull;
+
+public class ExprMongoCollectionDocCount extends SimplePropertyExpression<MongoSKCollection, Integer> {
+
+    static {
+        register(ExprMongoCollectionDocCount.class, Integer.class, "mongo[(db|sk)] [estimated] doc[ument][s] count", "mongoskcollections");
+    }
+
+    @Nonnull
+    @Override
+    public Integer convert(MongoSKCollection collection) {
+        SubscriberHelpers.ObservableSubscriber<Long> observableSubscriber = new SubscriberHelpers.OperationSubscriber<>();
+        collection.getMongoCollection().estimatedDocumentCount().subscribe(observableSubscriber);
+        return observableSubscriber.await().getReceived().get(0).intValue();
+    }
+
+    @Nonnull
+    @Override
+    protected String getPropertyName() {
+        return "mongo estimated document count";
+    }
+
+    @Nonnull
+    @Override
+    public Class<? extends Integer> getReturnType() {
+        return Integer.class;
+    }
+}
