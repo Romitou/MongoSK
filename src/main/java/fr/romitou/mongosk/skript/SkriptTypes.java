3a53efef364bbd3264e79e305b3e50c2cfc2ac69
Romitou <romitou@protonmail.com>
2021-03-21 20:25:04 +0100

    :sparkles: Add default expressions

--- src/main/java/fr/romitou/mongosk/skript/SkriptTypes.java
+++ src/main/java/fr/romitou/mongosk/skript/SkriptTypes.java
@@ -2,6 +2,7 @@ package fr.romitou.mongosk.skript;
 
 import ch.njol.skript.classes.ClassInfo;
 import ch.njol.skript.classes.Parser;
+import ch.njol.skript.expressions.base.EventValueExpression;
 import ch.njol.skript.lang.ParseContext;
 import ch.njol.skript.registrations.Classes;
 import fr.romitou.mongosk.elements.*;
@@ -12,6 +13,7 @@ public class SkriptTypes {
     static {
         Classes.registerClass(new ClassInfo<>(MongoSKServer.class, "mongoskserver")
             .user("mongo(db|sk)?( |-)?servers?")
+            .defaultExpression(new EventValueExpression<>(MongoSKServer.class))
             .name("MongoSK Server")
             .description("Represents a remote MongoDB server, with which you can interact. To create this type, use the Mongo Server expression.")
             .since("2.0.0")
@@ -43,6 +45,7 @@ public class SkriptTypes {
 
         Classes.registerClass(new ClassInfo<>(MongoSKDatabase.class, "mongoskdatabase")
             .user("mongo(db|sk)?( |-)?databases?")
+            .defaultExpression(new EventValueExpression<>(MongoSKDatabase.class))
             .name("MongoSK Database")
             .description("Represents a Mongo database, which contains collections. If you want to create this type, look at the Mongo Database expression.")
             .since("2.0.0")
@@ -74,6 +77,7 @@ public class SkriptTypes {
 
         Classes.registerClass(new ClassInfo<>(MongoSKCollection.class, "mongoskcollection")
             .user("mongo(db|sk)?( |-)?collections?")
+            .defaultExpression(new EventValueExpression<>(MongoSKCollection.class))
             .name("MongoSK Collection")
             .description("Represents a Mongo collection, which contains documents. Use the Mongo Collection expression to create this type.")
             .since("2.0.0")
@@ -105,10 +109,12 @@ public class SkriptTypes {
 
         Classes.registerClass(new ClassInfo<>(MongoSKDocument.class, "mongoskdocument")
             .user("mongo(db|sk)?( |-)?documents?")
+            .defaultExpression(new EventValueExpression<>(MongoSKDocument.class))
             .name("MongoSK Document")
             .description("Represents a Mongo (BSON) document, which contains some fields and values. If you want to read fields values, use the Mongo Value expression. Otherwise, you can use the Mongo Query expression to query your Mongo collection for documents.")
             .since("2.0.0")
             .parser(new Parser<MongoSKDocument>() {
+
                 @Override
                 public boolean canParse(@Nonnull ParseContext context) {
                     return false;
@@ -136,6 +142,7 @@ public class SkriptTypes {
 
         Classes.registerClass(new ClassInfo<>(MongoSKFilter.class, "mongoskfilter")
             .user("mongo(db|sk)?( |-)?filters?")
+            .defaultExpression(new EventValueExpression<>(MongoSKFilter.class))
             .name("MongoSK Filter")
             .description("Represents a filter, which can be used in your queries to target a certain type of data. You can create a filter using the Mongo Filter expression. ")
             .since("2.0.0")
@@ -167,6 +174,7 @@ public class SkriptTypes {
 
         Classes.registerClass(new ClassInfo<>(MongoSKQuery.class, "mongoskquery")
             .user("mongo(db|sk)?( |-)?quer(y|ies)")
+            .defaultExpression(new EventValueExpression<>(MongoSKQuery.class))
             .name("MongoSK Query")
             .description("Allows you to build a complex query to target a very specific type of data with specific options.")
             .since("2.0.0")
@@ -199,6 +207,7 @@ public class SkriptTypes {
 
         Classes.registerClass(new ClassInfo<>(MongoSKSort.class, "mongosksort")
             .user("mongo(db|sk)?( |-)?sorts?")
+            .defaultExpression(new EventValueExpression<>(MongoSKSort.class))
             .name("MongoSK Sort")
             .description("Create a sort to first retrieve certain documents according to the specified fields.")
             .since("2.0.0")
