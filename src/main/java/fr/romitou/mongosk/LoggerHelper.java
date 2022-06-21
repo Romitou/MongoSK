9c461f164e7f40ab376f0cabb86ed0e29f1d9d3e
Romitou <romitou@protonmail.com>
2022-06-21 23:25:41 +0200

    :loud_sound: Remove logging colors

--- src/main/java/fr/romitou/mongosk/LoggerHelper.java
+++ src/main/java/fr/romitou/mongosk/LoggerHelper.java
@@ -1,7 +1,6 @@
 package fr.romitou.mongosk;
 
 import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
 
 import java.util.Arrays;
 import java.util.logging.Logger;
@@ -10,14 +9,10 @@ public class LoggerHelper {
 
     private final static Logger LOGGER = Bukkit.getLogger();
     private final static Boolean DEBUG = MongoSK.getInstance().getConfig().getBoolean("debug-mode", false);
-    private final static String PREFIX = "&7[&2Mongo&aSK&7] ";
-
-    public static String getFormattedString(String message) {
-        return ChatColor.translateAlternateColorCodes('&', PREFIX + message);
-    }
+    private final static String PREFIX = "[MongoSK] ";
 
     public static void info(String... messages) {
-        LOGGER.info(getFormattedString(ChatColor.WHITE + messages[0]));
+        LOGGER.info(PREFIX + messages[0]);
         if (messages.length > 1)
             Arrays.stream(messages)
                 .skip(1)
@@ -26,20 +21,15 @@ public class LoggerHelper {
 
     public static void debug(String... messages) {
         if (!DEBUG) return;
-        LOGGER.config(getFormattedString(ChatColor.DARK_GRAY + messages[0]));
+        LOGGER.config(PREFIX + messages[0]);
         if (messages.length > 1)
             Arrays.stream(messages)
                 .skip(1)
-                .forEachOrdered(msg -> System.out.println(getFormattedString(ChatColor.DARK_GRAY + "-> " + msg)));
-    }
-
-    private static void addDebugInfo(String message) {
-        if (!DEBUG) return;
-        LOGGER.config(getFormattedString(ChatColor.DARK_GRAY + "-> " + message));
+                .forEachOrdered(LoggerHelper::addDebugInfo);
     }
 
     public static void warn(String... messages) {
-        LOGGER.warning(getFormattedString(ChatColor.GOLD + messages[0]));
+        LOGGER.warning(PREFIX + messages[0]);
         if (messages.length > 1)
             Arrays.stream(messages)
                 .skip(1)
@@ -47,11 +37,16 @@ public class LoggerHelper {
     }
 
     public static void severe(String... messages) {
-        LOGGER.severe(getFormattedString(ChatColor.RED + messages[0]));
+        LOGGER.severe(PREFIX + messages[0]);
         if (messages.length > 1)
             Arrays.stream(messages)
                 .skip(1)
                 .forEachOrdered(LoggerHelper::addDebugInfo);
+    }
+
+    private static void addDebugInfo(String message) {
+        if (!DEBUG) return;
+        LOGGER.config(PREFIX + "-> " + message);
     }
 
 }
