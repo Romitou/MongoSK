e830e944d8980a973c45c10a9523b1f54c58d2f1
romitou <romitou@protonmail.com>
2026-04-25 20:02:06 +0200

    :sparkles: Add upsert functionality to manage Mongo documents (#65)

--- src/main/java/fr/romitou/mongosk/skript/effects/EffManageMongoDocument.java
+++ src/main/java/fr/romitou/mongosk/skript/effects/EffManageMongoDocument.java
@@ -9,8 +9,10 @@ import ch.njol.skript.lang.Effect;
 import ch.njol.skript.lang.Expression;
 import ch.njol.skript.lang.SkriptParser;
 import ch.njol.util.Kleenean;
+import com.mongodb.client.model.DeleteOneModel;
 import com.mongodb.client.model.Filters;
 import com.mongodb.client.model.ReplaceOneModel;
+import com.mongodb.client.model.ReplaceOptions;
 import com.mongodb.bulk.BulkWriteResult;
 import com.mongodb.client.result.DeleteResult;
 import com.mongodb.client.result.InsertManyResult;
@@ -24,12 +26,16 @@ import org.bukkit.event.Event;
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 import java.util.Arrays;
+import java.util.List;
 import java.util.stream.Collectors;
+import org.bson.Document;
+import com.mongodb.client.model.WriteModel;
 
 @Name("Manage Mongo document")
 @Description("Firstly, you can insert your Mongo document in the collection you want, with an automatically generated " +
     "unique identifier if not specified or already existing. This will not replace existing documents with the same " +
     "identifier, but will insert a new one. Secondly, if you want to replace the document, you can use the replace syntax. " +
+    "Thirdly, you can use upsert to insert the document if it does not exist, or replace it if it does. " +
     "Finally, to remove the document, simply use remove. " +
     "" +
     "This effect is only a shortcut and simply executes a query with \"_id\" as the unique identifier. " +
@@ -39,6 +45,7 @@ import java.util.stream.Collectors;
     "insert mongo document {_document} into collection {mycollection}",
     "",
     "update mongo document {_document} of {mycollection}",
+    "upsert mongo document {_document} into {mycollection}",
     "remove mongo documents {_doc1} and {_doc2} from {mycollection}"})
 @Since("2.0.0")
 public class EffManageMongoDocument extends Effect {
@@ -46,7 +53,7 @@ public class EffManageMongoDocument extends Effect {
     static {
         Skript.registerEffect(
             EffManageMongoDocument.class,
-            "(1¦insert|2¦(replace|update)|3¦remove) mongo[(sk|db)] doc[ument][s] %mongoskdocuments% (in[to]|of|from) [collection] %mongoskcollection%"
+            "(1¦insert|2¦(replace|update)|3¦remove|4¦upsert) mongo[(sk|db)] doc[ument][s] %mongoskdocuments% (in[to]|of|from) [collection] %mongoskcollection%"
         );
     }
 
@@ -71,37 +78,60 @@ public class EffManageMongoDocument extends Effect {
             return;
         switch (parseMark) {
             case 1:
-                long insertQuery = System.currentTimeMillis();
-                SubscriberHelpers.ObservableSubscriber<InsertManyResult> observableInsertSubscriber = new SubscriberHelpers.OperationSubscriber<>();
-                mongoSKCollection.getMongoCollection()
-                    .insertMany(Arrays.stream(mongoSKDocuments)
-                        .map(MongoSKDocument::getBsonDocument)
-                        .collect(Collectors.toList()))
-                    .subscribe(observableInsertSubscriber);
-                observableInsertSubscriber.await();
-                LoggerHelper.debug("Insert query executed in " + (System.currentTimeMillis() - insertQuery) + "ms.");
+                List<Document> insertList = Arrays.stream(mongoSKDocuments)
+                    .map(MongoSKDocument::getBsonDocument)
+                    .collect(Collectors.toList());
+                if (!insertList.isEmpty()) {
+                    long insertQuery = System.currentTimeMillis();
+                    SubscriberHelpers.ObservableSubscriber<InsertManyResult> observableInsertSubscriber = new SubscriberHelpers.OperationSubscriber<>();
+                    mongoSKCollection.getMongoCollection()
+                        .insertMany(insertList)
+                        .subscribe(observableInsertSubscriber);
+                    observableInsertSubscriber.await();
+                    LoggerHelper.debug("Insert query executed in " + (System.currentTimeMillis() - insertQuery) + "ms.");
+                }
                 break;
             case 2:
-                long updateQuery = System.currentTimeMillis();
-                SubscriberHelpers.ObservableSubscriber<BulkWriteResult> observableUpdateSubscriber = new SubscriberHelpers.OperationSubscriber<>();
-                mongoSKCollection.getMongoCollection()
-                    .bulkWrite(Arrays.stream(mongoSKDocuments)
-                        .map(doc -> new ReplaceOneModel<>(Filters.eq("_id", doc.getBsonDocument().get("_id")), doc.getBsonDocument()))
-                        .collect(Collectors.toList()))
-                    .subscribe(observableUpdateSubscriber);
-                observableUpdateSubscriber.await();
-                LoggerHelper.debug("Update query executed in " + (System.currentTimeMillis() - updateQuery) + "ms.");
+                List<WriteModel<Document>> updateList = Arrays.stream(mongoSKDocuments)
+                    .map(doc -> (WriteModel<Document>) new ReplaceOneModel<Document>(Filters.eq("_id", doc.getBsonDocument().get("_id")), doc.getBsonDocument()))
+                    .collect(Collectors.toList());
+                if (!updateList.isEmpty()) {
+                    long updateQuery = System.currentTimeMillis();
+                    SubscriberHelpers.ObservableSubscriber<BulkWriteResult> observableUpdateSubscriber = new SubscriberHelpers.OperationSubscriber<>();
+                    mongoSKCollection.getMongoCollection()
+                        .bulkWrite(updateList)
+                        .subscribe(observableUpdateSubscriber);
+                    observableUpdateSubscriber.await();
+                    LoggerHelper.debug("Update query executed in " + (System.currentTimeMillis() - updateQuery) + "ms.");
+                }
                 break;
             case 3:
-                long deleteQuery = System.currentTimeMillis();
-                Arrays.stream(mongoSKDocuments).forEachOrdered(doc -> {
-                    SubscriberHelpers.ObservableSubscriber<DeleteResult> observableDeleteSubscriber = new SubscriberHelpers.OperationSubscriber<>();
+                List<WriteModel<Document>> deleteList = Arrays.stream(mongoSKDocuments)
+                    .map(doc -> (WriteModel<Document>) new DeleteOneModel<Document>(Filters.eq("_id", doc.getBsonDocument().get("_id"))))
+                    .collect(Collectors.toList());
+                if (!deleteList.isEmpty()) {
+                    long deleteQuery = System.currentTimeMillis();
+                    SubscriberHelpers.ObservableSubscriber<BulkWriteResult> observableDeleteSubscriber = new SubscriberHelpers.OperationSubscriber<>();
                     mongoSKCollection.getMongoCollection()
-                        .deleteOne(Filters.eq("_id", doc.getBsonDocument().get("_id")))
+                        .bulkWrite(deleteList)
                         .subscribe(observableDeleteSubscriber);
                     observableDeleteSubscriber.await();
-                });
-                LoggerHelper.debug("Delete query executed in " + (System.currentTimeMillis() - deleteQuery) + "ms.");
+                    LoggerHelper.debug("Delete query executed in " + (System.currentTimeMillis() - deleteQuery) + "ms.");
+                }
+                break;
+            case 4:
+                List<WriteModel<Document>> upsertList = Arrays.stream(mongoSKDocuments)
+                    .map(doc -> (WriteModel<Document>) new ReplaceOneModel<Document>(Filters.eq("_id", doc.getBsonDocument().get("_id")), doc.getBsonDocument(), new ReplaceOptions().upsert(true)))
+                    .collect(Collectors.toList());
+                if (!upsertList.isEmpty()) {
+                    long upsertQuery = System.currentTimeMillis();
+                    SubscriberHelpers.ObservableSubscriber<BulkWriteResult> observableUpsertSubscriber = new SubscriberHelpers.OperationSubscriber<>();
+                    mongoSKCollection.getMongoCollection()
+                        .bulkWrite(upsertList)
+                        .subscribe(observableUpsertSubscriber);
+                    observableUpsertSubscriber.await();
+                    LoggerHelper.debug("Upsert query executed in " + (System.currentTimeMillis() - upsertQuery) + "ms.");
+                }
                 break;
         }
 
